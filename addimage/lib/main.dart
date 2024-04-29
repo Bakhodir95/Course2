6677a34d0106b42bad04292b349b535f3a31ce1a
Bahodir <bakhodir.turdimurodov@bk.ru>
2024-04-29 18:39:32 +0500

    first c

--- addimage/lib/main.dart
+++ addimage/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -16,6 +18,7 @@ class MyApp extends StatelessWidget {
             onPressed: () {},
             icon: Image.asset(
               "assets/images/donut.png",
+              scale: 10,
             ),
             style: IconButton.styleFrom(backgroundColor: Colors.grey.shade300),
           ),
@@ -31,7 +34,9 @@ class MyApp extends StatelessWidget {
                 onPressed: () {},
                 icon: const Icon(Icons.notification_important),
                 style: IconButton.styleFrom(
-                    backgroundColor: Colors.blueGrey.shade100, iconSize: 40),
+                  backgroundColor: Colors.blueGrey.shade100,
+                  iconSize: 35,
+                ),
               ),
             )
           ],
@@ -39,6 +44,7 @@ class MyApp extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.only(right: 30, left: 30, top: 20),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
                 width: 400,
@@ -64,7 +70,7 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               const SizedBox(
-                height: 20,
+                height: 30,
               ),
               const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -80,16 +86,23 @@ class MyApp extends StatelessWidget {
                           color: Colors.orangeAccent))
                 ],
               ),
+              const SizedBox(
+                height: 10,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
                     children: [
                       Image.asset(
-                        "assets/images/koala.png",
+                        "assets/images/penguin.png",
+                        fit: BoxFit.cover,
                         scale: 10,
                       ),
-                      Text("Teacher"),
+                      const Text(
+                        "Penguin",
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ],
                   ),
                   Column(
@@ -98,32 +111,260 @@ class MyApp extends StatelessWidget {
                         "assets/images/koala.png",
                         scale: 10,
                       ),
-                      Text("Teacher"),
+                      const Text(
+                        "Koala",
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ],
                   ),
                   Column(
                     children: [
                       Image.asset(
-                        "assets/images/koala.png",
+                        "assets/images/dragon.png",
                         scale: 10,
                       ),
-                      Text("Teacher"),
+                      const Text(
+                        "Dragon",
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ],
                   ),
                   Column(
                     children: [
                       Image.asset(
-                        "assets/images/koala.png",
-                        scale: 10,
+                        "assets/images/donut.png",
+                        scale: 8,
                       ),
-                      Text("Teacher"),
+                      const Text(
+                        "Pony",
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ],
                   ),
                 ],
               ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                "Courses",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "All",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    "Design",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    "Programming",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    "Gaming",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              const Stack(
+                children: [
+                  Divider(
+                    color: Colors.grey,
+                    endIndent: 1,
+                  ),
+                  Divider(
+                    color: Colors.red,
+                    indent: 2,
+                    endIndent: 360,
+                    thickness: 5,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Flexible(
+                child: SingleChildScrollView(
+                    child: Column(
+                  children: [
+                    BottomPart(
+                        image: "assets/images/images (1).jpg",
+                        icon1: const Icon(
+                          Icons.note_alt,
+                          color: Colors.blue,
+                          size: 30,
+                        ),
+                        text1: "24 Lessons",
+                        icon2: const Icon(
+                          Icons.place,
+                          color: Colors.orange,
+                          size: 30,
+                        ),
+                        text2: "2Hr 30Min",
+                        textLast: "Learn Web Development"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    BottomPart(
+                        image: "assets/images/images.jpg",
+                        icon1: const Icon(
+                          Icons.note_alt,
+                          color: Colors.blue,
+                          size: 30,
+                        ),
+                        text1: "24 Lessons",
+                        icon2: const Icon(
+                          Icons.place,
+                          color: Colors.orange,
+                          size: 30,
+                        ),
+                        text2: "2Hr 30Min",
+                        textLast: "Learn Front-End"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    BottomPart(
+                        image: "assets/images/images (2).jpg",
+                        icon1: const Icon(
+                          Icons.note_alt,
+                          color: Colors.blue,
+                          size: 30,
+                        ),
+                        text1: "24 Lessons",
+                        icon2: const Icon(
+                          Icons.place,
+                          color: Colors.orange,
+                          size: 30,
+                        ),
+                        text2: "2Hr 30Min",
+                        textLast: "Learn Python New"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    BottomPart(
+                        image: "assets/images/download.jpg",
+                        icon1: const Icon(
+                          Icons.note_alt,
+                          color: Colors.blue,
+                          size: 30,
+                        ),
+                        text1: "24 Lessons",
+                        icon2: const Icon(
+                          Icons.place,
+                          color: Colors.orange,
+                          size: 30,
+                        ),
+                        text2: "2Hr 30Min",
+                        textLast: "Learn UI and UX"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                )),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class BottomPart extends StatelessWidget {
+  final String image;
+  final Icon icon1;
+  final String text1;
+  final Icon icon2;
+  final String text2;
+  final String textLast;
+
+  BottomPart({
+    required this.image,
+    required this.icon1,
+    required this.text1,
+    required this.icon2,
+    required this.text2,
+    required this.textLast,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(7),
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              height: 100,
+              padding: const EdgeInsets.all(13),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 3,
+                        spreadRadius: 3,
+                        color: Colors.grey.shade500)
+                  ],
+                  color: Colors.orange.shade100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(height: 50, width: 50, child: Image.asset(image)),
+                  Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            icon1,
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              text1,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            icon2,
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              text2,
+                              style: TextStyle(fontSize: 20),
+                            )
+                          ],
+                        ),
+                        Text(
+                          textLast,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
